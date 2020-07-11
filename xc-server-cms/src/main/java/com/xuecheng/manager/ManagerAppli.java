cf25b43f0c1f3f18f6af074e3cf967223b818406
SoftCancer <ygxzzu@163.com>
2020-07-11 21:05:41 +0800

    声明 RestTemplate 实例，用于远程调用

--- xc-server-cms/src/main/java/com/xuecheng/manager/ManagerAppli.java
+++ xc-server-cms/src/main/java/com/xuecheng/manager/ManagerAppli.java
@@ -3,7 +3,10 @@ package com.xuecheng.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Description:
@@ -20,5 +23,10 @@ public class ManagerAppli {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerAppli.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
 }
